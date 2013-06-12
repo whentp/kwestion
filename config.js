bdbac75ba48cc47144639fe2baaf0c068cfc8f33
whentp <tpsmsproject@gmail.com>
2013-06-13 00:33:02 +0800

    use entities.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // vim:ft=javascript:tabstop=2:shiftwidth=2:softtabstop=2:expandtab
 
 var k_config = {
-  version : '5.1.10',
+  version : '5.1.11',
   twitter_consumer_key : 'cEik18NpLALwbgTLz0pefQ',
   twitter_consumer_secret : '5p47cbmygL0xaBOzTexTainxBZPfprte0wa6ERjEays',
   twitter_api_prefix : 'https://api.twitter.com/1.1/', // all url prefix should end with a slash.
